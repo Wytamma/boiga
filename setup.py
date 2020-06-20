576a977c486b60132197e7a09fb5226128748849
Wytamma Wirth <wytamma.wirth@me.com>
2020-06-21 01:05:05 +1000

    :art: auto inc version

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 import setuptools
-
+import requests
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+r = requests.get('https://pypi.org/pypi/pyoinformatics/json')
+version = r.json()['info']['version'].split('.')
+patch_version = str(int(version[-1]) + 1)
+if patch_version == '9':
+    patch_version = '0'
+    version[1] = str(int(version[1]) + 1)
+
 setuptools.setup(
     name="pyoinformatics", # Replace with your own username
-    version="0.0.2",
+    version='.'.join(version[:-1] + [patch_version]),
     author="Wytamma Wirth",
     author_email="wytamma.wirth@me.com",
     description="A simple bioinformatics package",
